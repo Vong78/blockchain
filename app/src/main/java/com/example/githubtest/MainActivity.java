9f19a93dcf1af1820931e526bccc4ed676c6ef81
vongyuhshin <yuhshin78@gmail.com>
2020-07-21 18:06:09 +0800

    V1

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("loading"," Not OK");
+        System.out.println("OK");
     }
 }
